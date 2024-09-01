252823e0f3daad827854109b29b1b552175341a6
Siddalingaraju D <66860692+sid-jpg@users.noreply.github.com>
2024-09-01 16:13:44 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,18 +5,18 @@ const Hero = () => {
   return (
     <section className="hero bg-gray-100 dark:bg-gray-900 text-black dark:text-white min-h-screen flex items-center">
       <div className="container mx-auto px-4 text-left">
-        <h1 className="mb-3 md:mb-8">
-          <span className="block text-4xl md:text-8xl lg:text-9xl font-bold">Hi,</span>
-          <span className="block text-3xl md:text-6xl lg:text-7xl font-bold">
+        <h1 className="mb-4 md:mb-6 lg:mb-8">
+          <span className="block text-2xl md:text-4xl lg:text-5xl font-bold">Hi,</span>
+          <span className="block text-xl md:text-3xl lg:text-4xl font-bold">
             I'm <span className="text-blue-500 dark:text-techno-blue">S</span>iddalingaraju,
           </span>
-          <span className="block text-2xl md:text-5xl lg:text-6xl font-bold">computer science &</span>
-          <span className="block text-2xl md:text-5xl lg:text-6xl font-bold text-techno-blue dark:text-techno-blue">engineering student</span>
+          <span className="block text-lg md:text-2xl lg:text-3xl font-bold">computer science &</span>
+          <span className="block text-lg md:text-2xl lg:text-3xl font-bold text-techno-blue dark:text-techno-blue">engineering student</span>
         </h1>
-        <p className="text-lg md:text-xl mb-6 md:mb-8">
+        <p className="text-sm md:text-base lg:text-lg mb-4 md:mb-6">
           Cybersecurity Enthusiast / Back-end Developer
         </p>
-        <div className="flex flex-col md:flex-row justify-start space-y-4 md:space-y-0 md:space-x-4 mb-8">
+        <div className="flex flex-col md:flex-row justify-start space-y-3 md:space-y-0 md:space-x-4 mb-6">
           <button
             onClick={() => window.location.href = '#contact'}
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
@@ -32,10 +32,10 @@ const Hero = () => {
         </div>
         <div className="flex justify-start space-x-4">
           <a href="https://www.linkedin.com/in/siddalingaraju-d-829a93257/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin className="text-3xl md:text-4xl text-black dark:text-white hover:text-blue-600" />
+            <FaLinkedin className="text-xl md:text-2xl lg:text-3xl text-black dark:text-white hover:text-blue-600" />
           </a>
           <a href="https://github.com/sid-jpg" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="text-3xl md:text-4xl text-black dark:text-white hover:text-gray-600" />
+            <FaGithub className="text-xl md:text-2xl lg:text-3xl text-black dark:text-white hover:text-gray-600" />
           </a>
         </div>
       </div>
